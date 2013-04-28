c9439176a6e29a8883891fbd073b8bd7b227c087
Varvara Stepanova <toivonen@toivonen.ru>
2013-04-28 19:56:24 +0200

    Layout blocks on a page

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -10,12 +10,29 @@
     ],
     content:[
         {
+            block: 'header',
+            content: 'h'
+        },
+        {
             block: 'layout',
             content: [
-                { block: 'main' },
-                { block: 'sidebar' },
-                { block: 'prompting' }
+                {
+                    block: 'main',
+                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                },
+                {
+                    block: 'sidebar',
+                    content: 's'
+                },
+                {
+                    block: 'prompting',
+                    content: 'p'
+                }
             ]
+        },
+        {
+            block: 'footer',
+            content: 'f'
         },
         { block: 'custom-fonts' },
         { block: 'i-jquery', mods: { version: '1.8.3' } },
