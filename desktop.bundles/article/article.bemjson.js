339cf5d98f5ce9e2d6f503af23fd40aeace5e554
Varvara Stepanova <toivonen@toivonen.ru>
2013-04-29 01:17:32 +0200

    Widgets on a page

--- desktop.bundles/article/article.bemjson.js
+++ desktop.bundles/article/article.bemjson.js
@@ -18,11 +18,57 @@
             content: [
                 {
                     block: 'main',
-                    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                    content: [
+                        {
+                            block: 'article',
+                            title: 'The Case of The Wonderful Button',
+                            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+                        }
+                    ]
                 },
                 {
                     block: 'sidebar',
-                    content: 's'
+                    content: [
+                        {
+                            block: 'b-menu-vert',
+                            mix: [{ block: 'box' }, { block: 'box', elem: 'body'}],
+                            content: [
+                                {
+                                    elem: 'title',
+                                    mix: { block: 'box', elem: 'title'},
+                                    content: 'On this site'
+                                },
+                                {
+                                    elem: 'item',
+                                    content: {
+                                        block: 'b-link',
+                                        url: '#',
+                                        content: 'Blog'
+                                    }
+                                },
+                                {
+                                    elem: 'item',
+                                    content: {
+                                        block: 'b-link',
+                                        url: '#',
+                                        content: 'Articles and talks'
+                                    }
+                                },
+                                {
+                                    elem: 'item',
+                                    content: {
+                                        block: 'b-link',
+                                        url: '#',
+                                        content: 'About me'
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            block: 'github',
+                            js: { user: 'toivonen' }
+                        }
+                    ]
                 },
                 {
                     block: 'prompting',
